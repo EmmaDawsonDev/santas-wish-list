14eda1f75d1a16ef2c9a9967a958ac797198c7bd
Emma Dawson <emma.l.dawson@gmail.com>
2020-12-14 17:02:37 +0100

    added deployment code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import VueAnime from "vue-animejs";
 
-createApp(App).mount("#app");
-App.use(VueAnime);
+const app = createApp(App);
+
+app.mount("#app");
