4dca02b9a62f804dea9b788c0646bca042dcb7e4
Emma Dawson <emma.l.dawson@gmail.com>
2020-12-14 13:26:25 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,140 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="list">
+      <h1>Dear Santa,</h1>
+      <h3>This year I have been very good. Here is my wish list:</h3>
+      <input
+        type="text"
+        name="wishlistItem"
+        placeholder="Write your wish here"
+        v-model="wishlistItem"
+        @keyup.enter="addToWishList"
+      />
+      <button class="add-item-btn" @click="addToWishList">Add item</button>
+      <section class="list-items">
+        <wish-list-item
+          v-for="(item, index) in wishList"
+          :key="index"
+          :id="index"
+          :listItem="item"
+          @deleteItem="removeFromList(index)"
+        ></wish-list-item>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WishListItem from "./components/WishListItem.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      wishlistItem: "",
+      wishList: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    WishListItem,
+  },
+  methods: {
+    addToWishList() {
+      this.wishList.push(this.wishlistItem);
+      this.wishlistItem = "";
+    },
+    removeFromList(id) {
+      this.wishList.splice(id, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url("https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  height: 100%;
+  font-family: "Gochi Hand", cursive;
+  color: rgb(146, 19, 19);
+}
+.wrapper {
+  height: 100vh;
+  max-width: 1000px;
+  margin: 0 auto;
+  background-image: url("../public/images/scroll.png");
+  background-repeat: no-repeat;
+  background-size: 98% 98%;
+  background-position: center;
+}
+
+.list {
+  width: 60%;
+  margin-left: 30%;
+  padding-top: 50px;
+  height: 74vh;
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+h3 {
+  font-size: 1.7rem;
+}
+
+input {
+  background: none;
+  border: none;
+  border-bottom: 2px solid rgb(146, 19, 19);
+  margin-top: 1rem;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 1.7rem;
+  font-family: "Gochi Hand", cursive;
+}
+
+input::placeholder {
+  font-size: 1.2rem;
+}
+
+input:focus {
+  outline: 2px solid rgb(146, 19, 19);
+}
+
+button {
+  background-color: rgb(146, 19, 19);
+  color: white;
+  padding: 0.4rem 0.7rem;
+  font-size: 1.2rem;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-family: "Gochi Hand", cursive;
+  font-size: 1.4rem;
+  letter-spacing: 1.2px;
+  text-transform: uppercase;
+}
+
+button:hover {
+  background-color: rgb(98, 20, 20);
+}
+
+.add-item-btn {
+  display: flex;
+  align-self: flex-end;
+  margin-top: 1rem;
+}
+
+.list-items {
+  height: 100%;
+  overflow-x: hidden;
 }
 </style>
---
+++ src/components/WishListItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <p>{{ listItem }}</p>
+    <span @click="removeItem(id)">X</span>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    listItem: String,
+    id: Number,
+  },
+  methods: {
+    removeItem(index) {
+      this.$emit("deleteItem", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  width: 75%;
+
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+}
+
+p,
+span {
+  font-size: 1.7rem;
+}
+span:hover {
+  color: rgb(98, 20, 20);
+  cursor: pointer;
+}
+</style>
